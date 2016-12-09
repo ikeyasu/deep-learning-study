a67b0caf2a53648d4ecf2c6653a13418a450aa81
Yasuki Ikeuchi <ikeyasu@gmail.com>
2016-12-09 13:44:46 +0900

    [imp] atari block

--- src/open_ai_gym/train.py
+++ src/open_ai_gym/train.py
@@ -13,7 +13,7 @@ gpu_device = 0
 cuda.get_device(gpu_device).use()
 xp = cuda.cupy
 
-class Agent(chainer.Chain):
+class LinearAgent(chainer.Chain):
     gamma = 0.99
     initial_epsilon = 1
     epsilon_reduction = 0.001
@@ -21,11 +21,11 @@ class Agent(chainer.Chain):
 
     def __init__(self, input_size, output_size, hidden_size):
         initialW = chainer.initializers.HeNormal(0.01)
-        super(Agent, self).__init__(
+        super(LinearAgent, self).__init__(
             fc1=F.Linear(input_size, hidden_size, initialW=initialW),
             fc2=F.Linear(hidden_size, hidden_size, initialW=initialW),
             fc3=F.Linear(hidden_size, output_size, initialW=initialW),
-        )
+       )
         self.epsilon = self.initial_epsilon
         self.output_size = output_size
 
@@ -49,7 +49,59 @@ class Agent(chainer.Chain):
     def normalize_state(self, state):
         return xp.asarray(state, dtype=xp.float32)
 
-class CartPoleAgent(Agent):
+
+class CNNAgent(chainer.Chain):
+    gamma = 0.99
+    initial_epsilon = 1
+    epsilon_reduction = 0.001
+    min_epsilon = 0.01
+
+    def __init__(self, width, height, channel, action_size, latent_size):
+        feature_width = width
+        feature_height = height
+        for i in range(4):
+            feature_width = (feature_width + 1) // 2
+            feature_height = (feature_height + 1) // 2
+        feature_size = feature_width * feature_height * 64
+        super(CNNAgent, self).__init__(
+            conv1=L.Convolution2D(channel, 16, 8, stride=4, pad=3),
+            conv2=L.Convolution2D(16, 32, 5, stride=2, pad=2),
+            conv3=L.Convolution2D(32, 64, 5, stride=2, pad=2),
+            lstm=L.LSTM(feature_size, latent_size),
+            q=L.Linear(latent_size, action_size),
+        )
+        self.width = width
+        self.height = height
+        self.latent_size = latent_size
+        self.epsilon = self.initial_epsilon
+        self.output_size = action_size
+
+    def __call__(self, x, train=True):
+        h1 = F.relu(self.conv1(x))
+        h2 = F.relu(self.conv2(h1))
+        h3 = F.relu(self.conv3(h2))
+        h4 = self.lstm(h3)
+        q = self.q(h4)
+        return q
+
+    def reset_state(self):
+        self.lstm.reset_state()
+
+    def randomize_action(self, action):
+        if random.random() < self.epsilon:
+            return random.randint(0, self.output_size - 1)
+        return action
+
+    def reduce_epsilon(self):
+        self.epsilon = (self.epsilon - self.min_epsilon) * (1 - self.epsilon_reduction) + self.min_epsilon
+
+    def adjust_reward(self, state, reward, done):
+        return reward
+
+    def normalize_state(self, state):
+        return np.asarray(state, dtype=np.float32)
+
+class CartPoleAgent(LinearAgent):
     gamma = 0.9
     initial_epsilon = 1
     min_epsilon = 0.01
@@ -65,7 +117,7 @@ class CartPoleAgent(Agent):
         scale = xp.asarray([1 / 2.4, 1 / 4.0, 1 / 0.2, 1 / 3.0], dtype=xp.float32)
         return xp.asarray(state, dtype=xp.float32) * scale
 
-class MountainCarAgent(Agent):
+class MountainCarAgent(LinearAgent):
     gamma = 0.99
     initial_epsilon = 0.8
     min_epsilon = 0.1
@@ -80,6 +132,26 @@ class MountainCarAgent(Agent):
     def normalize_state(self, state):
         scale = xp.asarray([1 / 1.2, 1 / 0.07], dtype=xp.float32)
         return xp.asarray(state, dtype=xp.float32) * scale
+
+
+class Breakout(CNNAgent):
+    gamma = 0.99
+    initial_epsilon = 0.8
+    min_epsilon = 0.1
+    epsilon_reduction = 0.0001
+
+    def __init__(self):
+        #h ttps://gym.openai.com/envs/Breakout-v0
+        super(Breakout, self).__init__(210, 160, 3, 6, 256)
+
+    def adjust_reward(self, state, reward, done):
+        return reward
+
+    def normalize_state(self, state):
+        array = np.asarray(state, dtype=np.float32).transpose((2, 0, 1))
+        reshaped = array.reshape((1,) + array.shape)
+        return reshaped
+        #return array#chainer.Variable(array.reshape((1,) + state.shape) / 127.5 - 1, volatile=True)
 
 class ExperiencePool(object):
 
@@ -150,7 +222,7 @@ def update(agent, target_agent, optimizer, ex_pool, batch_size):
 
 def parse_arg():
     parser = argparse.ArgumentParser('Open AI Gym learning sample')
-    parser.add_argument('--env', '-e', type=str, choices=['cart_pole', 'mountain_car'], help='Environment name')
+    parser.add_argument('--env', '-e', type=str, choices=['cart_pole', 'mountain_car', 'breakout'], help='Environment name')
     parser.add_argument('--skip_render', '-s', type=int, default=0, help='Episodes nterval to skip rendering')
     parser.add_argument('--batch-size', '-b', type=int, default=32, help='Batch size for taining')
     parser.add_argument('--pool-size', '-p', type=int, default=2000, help='Experiance pool size')
@@ -175,6 +247,9 @@ def main():
     if env_name == 'mountain_car':
         env = gym.make('MountainCar-v0')
         agent = MountainCarAgent()
+    elif env_name == 'breakout':
+        env = gym.make('Breakout-v0')
+        agent = Breakout()
     else:
         env = gym.make('CartPole-v0')
         agent = CartPoleAgent()
@@ -187,7 +262,10 @@ def main():
         target_agent = agent
     optimizer = chainer.optimizers.Adam()
     optimizer.setup(agent)
-    ex_pool = ExperiencePool(pool_size, env.observation_space.shape)
+    shape = env.observation_space.shape
+    if env_name == 'breakout':
+        shape = (shape[2], shape[0], shape[1])
+    ex_pool = ExperiencePool(pool_size, shape)
 
     for episode in six.moves.range(episode_num):
         raw_state = env.reset()
@@ -196,7 +274,10 @@ def main():
         for t in six.moves.range(episode_length):
             if need_render:
                 env.render()
-            action = xp.argmax(agent(xp.expand_dims(state, 0)).data)
+            if env_name == 'breakout':
+                action = xp.argmax(agent(state).data)
+            else:
+                action = xp.argmax(agent(xp.expand_dims(state, 0)).data)
             action = agent.randomize_action(action)
 
             prev_state = state
@@ -218,3 +299,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
